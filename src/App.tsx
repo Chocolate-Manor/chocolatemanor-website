dc785423a7170a97d9d82bb623dfd0c0b36aec46
Haocheng Yang <alexyanghaocheng@gmail.com>
2025-03-28 10:33:18 +0000

    finish list group

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import ListGroup from "./components/ListGroup";
 
 function App() {
+  let items = ["A for Aletta", "Lone Star Ride"];
+
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
   return (
     <div>
-      <ListGroup />
+      <ListGroup
+        items={items}
+        heading="Games"
+        onSelectItem={handleSelectItem}
+      />
     </div>
   );
 }
