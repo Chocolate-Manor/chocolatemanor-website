9cf6006f09792cda40a8ad6d6b6f9d383d998855
Haocheng Yang <alexyanghaocheng@gmail.com>
2025-03-29 17:47:22 +0000

    install chakra UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,16 @@
-import Alert from "./components/Alert";
-import Button from "./components/Button";
-import ListGroup from "./components/ListGroup";
+import { Button, ButtonGroup } from "@chakra-ui/react";
 
 function App() {
-  let items = ["A for Aletta", "Lone Star Ride"];
-
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
   return (
-    <div>
-      <ListGroup
-        items={items}
-        heading="Games"
-        onSelectItem={handleSelectItem}
-      />
-      <Alert>
-        what<span>now</span>
-      </Alert>
+    <>
+      <Button colorScheme="blue">Button</Button>
       <Button
-        color="link"
-        onClick={() => console.log("clicked")}
+        colorScheme="teal"
+        variant="outline"
       >
-        what the heck dude
+        Button
       </Button>
-    </div>
+    </>
   );
 }
 
